049c0b40933033595280c58dfe3c1649572901f4
seuriseuljjeok <heeseul09@naver.com>
2024-01-04 23:26:54 +0900

    [Feat] 투두 컬렉션뷰 헤더 & 스티키헤더 구현

--- Going-iOS/Scene/OurToDo/Views/UnderlineSegmentedControlView.swift
+++ Going-iOS/Scene/OurToDo/Views/UnderlineSegmentedControlView.swift
@@ -1,20 +1,68 @@
-//
-//  UnderlineSegmentedControlView.swift
-//  Going-iOS
-//
-//  Created by 윤희슬 on 1/4/24.
-//
-
 import UIKit
 
-class UnderlineSegmentedControlView: UIView {
+class UnderlineSegmentedControlView: UISegmentedControl {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: - UI Property
+    
+    private lazy var underlineView: UIView = {
+        let width = absoluteWidth * 375 / 2
+        let height = absoluteHeight * 2
+        let xPosition = CGFloat(self.selectedSegmentIndex) * width
+        let yPosition = self.bounds.size.height - 2.0
+        print("y \(yPosition)")
+        let frame = CGRect(x: xPosition, y: yPosition, width: width, height: CGFloat(height))
+        let view = UIView(frame: frame)
+        view.backgroundColor = .gray500
+        self.addSubview(view)
+        return view
+    }()
+    
+    // MARK: - Property
+    
+    let absoluteWidth = UIScreen.main.bounds.width / 375
+    let absoluteHeight = UIScreen.main.bounds.height / 812
+    
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.removeBackgroundAndDivider()
     }
-    */
 
+    override init(items: [Any]?) {
+        super.init(items: items)
+        self.removeBackgroundAndDivider()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let underlineFinalXPosition = underlineView.bounds.width * CGFloat(self.selectedSegmentIndex)
+        UIView.animate(
+          withDuration: 0.1,
+          animations: {
+            self.underlineView.frame.origin.x = underlineFinalXPosition
+          }
+        )
+        print(underlineFinalXPosition)
+    }
 }
+
+
+// MARK: - private method
+
+private extension UnderlineSegmentedControlView {
+    func removeBackgroundAndDivider() {
+        let image = UIImage()
+        self.setBackgroundImage(image, for: .normal, barMetrics: .default)
+        self.setBackgroundImage(image, for: .selected, barMetrics: .default)
+        self.setBackgroundImage(image, for: .highlighted, barMetrics: .default)
+        
+        self.setDividerImage(image, forLeftSegmentState: .selected, rightSegmentState: .normal, barMetrics: .default)
+    }
+      
+}
+
