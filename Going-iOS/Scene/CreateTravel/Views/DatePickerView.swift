25a4f50f0659d06ca3cd26c3ded4fee258b02001
0seoYun <yys020819@gmail.com>
2024-01-05 03:29:01 +0900

    [Feat] #14 - DataPicker UIView 구현

---
+++ Going-iOS/Scene/CreateTravel/Views/DatePickerView.swift
@@ -0,0 +1,61 @@
+//
+//  DatePickerView.swift
+//  Going-iOS
+//
+//  Created by 윤영서 on 1/5/24.
+//
+
+import UIKit
+
+import SnapKit
+
+final class DatePickerView: UIView {
+    
+    // MARK: - UI Properties
+    
+    let datePicker: UIDatePicker = {
+        let picker = UIDatePicker()
+        picker.datePickerMode = .date
+        picker.isUserInteractionEnabled = true
+        picker.locale = Locale(identifier: "ko-KR")
+        if #available(iOS 13.4, *) {
+            picker.preferredDatePickerStyle = .wheels
+        }
+        return picker
+    }()
+        
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+                
+        setStyle()
+        setHierachy()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Private Methods
+
+private extension DatePickerView {
+    
+    func setStyle() {
+        self.backgroundColor = .white000
+    }
+    
+    func setHierachy() {
+        addSubview(datePicker)
+    }
+    
+    func setLayout() {
+        datePicker.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(37)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(safeAreaLayoutGuide).inset(80)
+        }
+    }
+}
