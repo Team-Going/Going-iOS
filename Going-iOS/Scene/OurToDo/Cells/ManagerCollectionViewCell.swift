7d864b41405c6dc706ac850cbc93bf8d081f18f1
seuriseuljjeok <heeseul09@naver.com>
2024-01-04 23:25:17 +0900

    [Feat] 아워투두 하단 투두 컬렉션뷰 구현

--- Going-iOS/Scene/OurToDo/Cells/ManagerCollectionViewCell.swift
+++ Going-iOS/Scene/OurToDo/Cells/ManagerCollectionViewCell.swift
@@ -1,12 +1,72 @@
-//
-//  ManagerCollectionViewCell.swift
-//  Going-iOS
-//
-//  Created by 윤희슬 on 1/4/24.
-//
 
 import UIKit
 
 class ManagerCollectionViewCell: UICollectionViewCell {
+    // MARK: - Property
     
+    static let identifier = "ManagerCollectionViewCell"
+    let absoluteWidth = UIScreen.main.bounds.width / 375
+    let absoluteHeight = UIScreen.main.bounds.height / 812
+    var manager: [String] = []
+    var managerData: String? {
+        didSet {
+            guard let managerData = managerData else {return}
+            print("cell \(managerData)")
+            self.managerLabel.text = managerData
+        }
+    }
+    
+    // MARK: - UI Properties
+    
+    var managerLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.pretendard(.detail2_regular)
+        label.textColor = UIColor.gray400
+        label.backgroundColor = UIColor.gray50
+        label.layer.cornerRadius = 4
+        label.layer.borderWidth = 0.75
+        label.layer.borderColor = UIColor.gray400.cgColor
+        label.textAlignment = .center
+        return label
+    }()
+
+    // MARK: - Life Cycle
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setHierachy()
+        setLayout()
+        setStyle()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func changeLabelColor(color: UIColor) {
+        managerLabel.textColor = color
+        managerLabel.layer.borderColor = color.cgColor
+    }
 }
+
+// MARK: - Private Method
+
+private extension ManagerCollectionViewCell {
+    
+    func setHierachy() {
+        contentView.addSubview(managerLabel)
+    }
+    
+    func setLayout() {
+        managerLabel.snp.makeConstraints{
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    func setStyle() {
+        managerLabel.text = self.managerData
+    }
+}
+
+
