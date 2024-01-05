d7a73574fe2603079005be373147dc8e689ebdf7
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-01-05 18:37:11 +0900

    [Feat] 유저테스트뷰 애니메이션 구현(#20)

--- Going-iOS/Scene/UserTest/ViewControllers/UserTestViewController.swift
+++ Going-iOS/Scene/UserTest/ViewControllers/UserTestViewController.swift
@@ -65,7 +65,6 @@ final class UserTestViewController: UIViewController {
         button.backgroundColor = .gray200
         button.isEnabled = false
         button.titleLabel?.font = .pretendard(.body1_bold)
-        button.setTitleColor(.gray200, for: .normal)
         button.layer.cornerRadius = 6
         return button
     }()
@@ -145,6 +144,7 @@ private extension UserTestViewController {
         
         testIndexLabel.text = "\(userTestDataStruct[index].testIndex) / 9"
         questionLabel.text = "\(userTestDataStruct[index].testTitle)"
+        
         firstButton.setTitle("\(userTestDataStruct[index].testText.firstQuest)", for: .normal)
         secondButton.setTitle("\(userTestDataStruct[index].testText.secondQuest)", for: .normal)
         thirdButton.setTitle("\(userTestDataStruct[index].testText.thirdQuest)", for: .normal)
@@ -153,8 +153,11 @@ private extension UserTestViewController {
     
     func updateNextButtonState() {
         // 선택된 버튼이 있는지 확인하고 nextButton의 활성화 여부를 결정
-        nextButton.isEnabled = selectedButton != nil
+        if selectedButton != nil {
+            nextButton.isEnabled = true
+        }
         nextButton.backgroundColor = nextButton.isEnabled ? .gray500 : .gray50
+        nextButton.titleLabel?.textColor = nextButton.isEnabled ? .white000 : .gray200
     }
     
     func resetButtons() {
@@ -168,13 +171,27 @@ private extension UserTestViewController {
         // 선택된 버튼 초기화
         selectedButton = nil
         
-        // nextButton 상태 갱신
-        updateNextButtonState()
     }
     
     func handleLastQuestion() {
         print(buttonIndexList)
         print("Push To NextVC")
+    }
+    
+    func setAnimation() {
+        let viewsToAnimate = [firstButton.titleLabel, secondButton.titleLabel,
+                              thirdButton.titleLabel, fourthButton.titleLabel]
+
+        UIView.animate(withDuration: 0.5, animations: {
+            viewsToAnimate.forEach { $0?.alpha = 0.0 }
+        }) { [self] _ in
+            // fade out 애니메이션 종료 후 실행될 코드
+            updateLabel()
+            
+            UIView.animate(withDuration: 0.5) {
+                viewsToAnimate.forEach { $0?.alpha = 1.0 }
+            }
+        }
     }
     
     @objc
@@ -189,6 +206,7 @@ private extension UserTestViewController {
         // 클릭안된 다른 버튼들의 색상 변경
         for case let button as UIButton in questionStackView.arrangedSubviews {
             button.backgroundColor = .gray50
+            button.layer.borderWidth = 0
         }
         
         // 클릭된 버튼의 색상 변경
@@ -201,24 +219,28 @@ private extension UserTestViewController {
         
         // nextButton 상태 갱신
         updateNextButtonState()
-        
+ 
     }
     
     @objc
     func nextButtonTapped() {
         
         if index < userTestDataStruct.count - 1 {
-            // 질문이 마지막이 아닌 경우
             
+            // 질문이 마지막이 아닌 경우
             testProgressView.setProgress(testProgressView.progress + 0.1111111, animated: true)
             index += 1
-            //            setAnimation()
+            setAnimation()
             resetButtons()
             buttonIndexList.append(self.buttonIndex)
             
+            // nextButton 상태 갱신
+            nextButton.backgroundColor = .gray50
+            nextButton.titleLabel?.textColor = .gray200
+            
         } else {
             // 질문이 마지막인 경우
-            //            setAnimation()
+            setAnimation()
             buttonIndexList.append(self.buttonIndex)
             handleLastQuestion()
         }
