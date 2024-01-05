67bc4814c2b59052a4788613fd80eb426d4a5fb8
seuriseuljjeok <heeseul09@naver.com>
2024-01-05 18:06:13 +0900

    [Add] 좌측 패딩 라벨 컴포넌트 추가

---
+++ Going-iOS/Global/UIComponents/LeftPaddingLabel.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+/// 좌측에 패딩이 있는 라벨 
+class LeftPaddingLabel: UILabel {
+    private var padding = UIEdgeInsets(top: 0.0, left: 12.0, bottom: 0.0, right: 12.0)
+
+        convenience init(padding: UIEdgeInsets) {
+            self.init()
+            self.padding = padding
+        }
+
+        override func drawText(in rect: CGRect) {
+            super.drawText(in: rect.inset(by: padding))
+        }
+
+        override var intrinsicContentSize: CGSize {
+            var contentSize = super.intrinsicContentSize
+            contentSize.height += padding.top + padding.bottom
+            contentSize.width += padding.left + padding.right
+
+            return contentSize
+        }
+}
