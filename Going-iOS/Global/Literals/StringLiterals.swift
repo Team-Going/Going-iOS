f5bf8bb468596a308a60137396c2fb177aed5fc8
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-01-05 14:44:42 +0900

    [Feat] 유저성향테스트스플래쉬뷰 구현(#20)

--- Going-iOS/Global/Literals/StringLiterals.swift
+++ Going-iOS/Global/Literals/StringLiterals.swift
@@ -12,4 +12,8 @@ enum StringLiterals {
         static let title = "우리만의 여행을 하다"
         static let personalInformation  = " 개인정보처리방침"
     }
+    
+    enum UserTest {
+        static let userTestSplashTitle = "9개의 질문으로\n여행 성향을 분석해보세요 "
+    }
 }
