97a65044ef6a9d5cc5455a932e95befb450699b8
0seoYun <yys020819@gmail.com>
2024-01-06 02:36:49 +0900

    [Add] CollectionView/TableView Cell Identifier 관련 extension 추가

---
+++ Going-iOS/Global/Extensions/UITableViewCell+.swift
@@ -0,0 +1,15 @@
+//
+//  UITableViewCell+.swift
+//  Going-iOS
+//
+//  Created by 윤영서 on 1/6/24.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    
+    static var cellIdentifier : String {
+        return String(describing: self)
+    }
+}
