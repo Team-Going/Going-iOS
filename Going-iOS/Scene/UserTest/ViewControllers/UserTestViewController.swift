34712a0b956ad28d329687f924cf37f4a52a56f7
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-01-05 17:21:38 +0900

    [Feat] 유저테스트뷰 기능 구현(#20)

--- Going-iOS/Scene/UserTest/ViewControllers/UserTestViewController.swift
+++ Going-iOS/Scene/UserTest/ViewControllers/UserTestViewController.swift
@@ -11,6 +11,16 @@ import SnapKit
 
 final class UserTestViewController: UIViewController {
     
+    private var buttonIndexList: [Int] = []
+    
+    private var index: Int = 0
+    
+    private var buttonIndex: Int = 0
+    
+    private let userTestDataStruct = UserTestQuestionStruct.dummy()
+    
+    private var selectedButton: UIButton?
+    
     private let testProgressView: UIProgressView = {
         let progress = UIProgressView()
         progress.progressTintColor = .red500
@@ -47,38 +57,40 @@ final class UserTestViewController: UIViewController {
     private lazy var secondButton: UIButton = makeButton()
     private lazy var thirdButton: UIButton = makeButton()
     private lazy var fourthButton: UIButton = makeButton()
-
+    
     private lazy var nextButton: UIButton = {
         let button = UIButton()
         button.setTitle("다음", for: .normal)
-//        button.addTarget(self, action: #selector(nextButtonTapped), for: .touchUpInside)
+        button.addTarget(self, action: #selector(nextButtonTapped), for: .touchUpInside)
         button.backgroundColor = .gray200
         button.isEnabled = false
         button.titleLabel?.font = .pretendard(.body1_bold)
-        button.titleLabel?.textColor = .gray200
+        button.setTitleColor(.gray200, for: .normal)
         button.layer.cornerRadius = 6
         return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setHierarchy()
         setLayout()
         setStyle()
+        updateNextButtonState()
+        updateLabel()
+        
     }
-
 }
 
 private extension UserTestViewController {
     
     func makeButton() -> UIButton {
         let button = UIButton()
-        //        button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+        button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
         button.backgroundColor = .gray50
         button.layer.cornerRadius = 12
         button.titleLabel?.font = .pretendard(.body3_medi)
-        button.titleLabel?.textColor = .gray500
+        button.setTitleColor(UIColor.gray500, for: .normal)
         return button
     }
     
@@ -119,12 +131,99 @@ private extension UserTestViewController {
             $0.leading.trailing.equalToSuperview().inset(24)
             $0.height.equalTo(ScreenUtils.getHeight(50))
         }
-        
-        
     }
     
     func setStyle() {
         view.backgroundColor = .white000
     }
     
+}
+
+private extension UserTestViewController {
+    
+    func updateLabel() {
+        
+        testIndexLabel.text = "\(userTestDataStruct[index].testIndex) / 9"
+        questionLabel.text = "\(userTestDataStruct[index].testTitle)"
+        firstButton.setTitle("\(userTestDataStruct[index].testText.firstQuest)", for: .normal)
+        secondButton.setTitle("\(userTestDataStruct[index].testText.secondQuest)", for: .normal)
+        thirdButton.setTitle("\(userTestDataStruct[index].testText.thirdQuest)", for: .normal)
+        fourthButton.setTitle("\(userTestDataStruct[index].testText.fourthQuest)", for: .normal)
+    }
+    
+    func updateNextButtonState() {
+        // 선택된 버튼이 있는지 확인하고 nextButton의 활성화 여부를 결정
+        nextButton.isEnabled = selectedButton != nil
+        nextButton.backgroundColor = nextButton.isEnabled ? .gray500 : .gray50
+    }
+    
+    func resetButtons() {
+        // 다른 버튼들의 색상을 클릭하지 않은 상태로 초기화
+        for case let button as UIButton in questionStackView.arrangedSubviews {
+            button.backgroundColor = .gray50
+            button.setTitleColor(UIColor.gray500, for: .normal)
+            button.layer.borderWidth = 0
+        }
+        
+        // 선택된 버튼 초기화
+        selectedButton = nil
+        
+        // nextButton 상태 갱신
+        updateNextButtonState()
+    }
+    
+    func handleLastQuestion() {
+        print(buttonIndexList)
+        print("Push To NextVC")
+    }
+    
+    @objc
+    func buttonTapped(_ sender: UIButton) {
+        
+        guard let buttonIndex = questionStackView.arrangedSubviews.firstIndex(of: sender) else {
+            return
+        }
+        print("Button Tapped: \(sender.title(for: .normal) ?? ""), Index: \(buttonIndex)")
+        self.buttonIndex = buttonIndex
+        
+        // 클릭안된 다른 버튼들의 색상 변경
+        for case let button as UIButton in questionStackView.arrangedSubviews {
+            button.backgroundColor = .gray50
+        }
+        
+        // 클릭된 버튼의 색상 변경
+        sender.backgroundColor = .white000
+        sender.layer.borderColor = UIColor.red500.cgColor
+        sender.layer.borderWidth = 1
+        sender.titleLabel?.textColor = .gray700
+        
+        selectedButton = sender
+        
+        // nextButton 상태 갱신
+        updateNextButtonState()
+        
+    }
+    
+    @objc
+    func nextButtonTapped() {
+        
+        if index < userTestDataStruct.count - 1 {
+            // 질문이 마지막이 아닌 경우
+            
+            testProgressView.setProgress(testProgressView.progress + 0.1111111, animated: true)
+            index += 1
+            //            setAnimation()
+            resetButtons()
+            buttonIndexList.append(self.buttonIndex)
+            
+        } else {
+            // 질문이 마지막인 경우
+            //            setAnimation()
+            buttonIndexList.append(self.buttonIndex)
+            handleLastQuestion()
+        }
+        if index == 8 {
+            nextButton.setTitle("제출하기", for: .normal)
+        }
+    }
 }
