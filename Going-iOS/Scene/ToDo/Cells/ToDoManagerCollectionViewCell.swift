0d3c1f81637ce25507d3e4cc4a021656e8290da3
seuriseuljjeok <heeseul09@naver.com>
2024-01-05 18:40:29 +0900

    [Feat] 투두 더미데이터 생성

---
+++ Going-iOS/Scene/ToDo/Cells/ToDoManagerCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ToDoManagerCollectionViewCell.swift
+//  Going-iOS
+//
+//  Created by 윤희슬 on 1/5/24.
+//
+
+import UIKit
+
+class ToDoManagerCollectionViewCell: UICollectionViewCell {
+    
+}
