076e9b9f69065c1d75e12b44bc92674e56ab04e9
0seoYun <yys020819@gmail.com>
2024-01-10 00:13:39 +0900

    [Feat] 설정뷰 내 Push / Present 구현 (#46)

---
+++ Going-iOS/Scene/Settings/ViewControllers/InquiryWebViewController.swift
@@ -0,0 +1,32 @@
+//
+//  InquiryWebViewController.swift
+//  Going-iOS
+//
+//  Created by 윤영서 on 1/9/24.
+//
+
+import UIKit
+
+import WebKit
+
+final class InquiryWebViewController: UIViewController {
+    
+    private var webView: WKWebView?
+    
+    override func loadView() {
+        super.loadView()
+        webView = WKWebView(frame: self.view.frame)
+        self.view = self.webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 나중에 URL 변경
+        let sURL = "https://useworld.github.io/iOS/webview/"
+        let uURL = URL(string: sURL)
+        let request = URLRequest(url: uURL!)
+        guard let web = webView else { return }
+        web.load(request)
+    }
+}
---
+++ Going-iOS/Scene/Settings/ViewControllers/PolicyWebViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PolicyWebViewController.swift
+//  Going-iOS
+//
+//  Created by 윤영서 on 1/10/24.
+//
+
+import UIKit
+
+import WebKit
+
+final class PolicyWebViewController: UIViewController {
+    
+    private var webView: WKWebView?
+    
+    override func loadView() {
+        super.loadView()
+        webView = WKWebView(frame: self.view.frame)
+        self.view = self.webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 나중에 URL 변경
+        let sURL = "https://useworld.github.io/iOS/webview/"
+        let uURL = URL(string: sURL)
+        let request = URLRequest(url: uURL!)
+        guard let web = webView else { return }
+        web.load(request)
+    }
+}
