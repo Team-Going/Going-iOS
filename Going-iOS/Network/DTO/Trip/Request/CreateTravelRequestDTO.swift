72f7fbbc606d0af5e4bd99e908d167f18cf79305
0seoYun <yys020819@gmail.com>
2024-01-13 02:42:34 +0900

    [Feat] 여행 생성 DTO 생성 (#89)

--- Going-iOS/Network/DTO/Trip/Request/CreateTravelRequestDTO.swift
+++ Going-iOS/Network/DTO/Trip/Request/CreateTravelRequestDTO.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 struct CreateTravelRequestDTO: Request, DTO {
-    let title, startDate, endDate: String
-    let styleA, styleB, styleC, styleD, styleE: Int
+    var title, startDate, endDate: String
+    var styleA, styleB, styleC, styleD, styleE: Int
 }
--- Going-iOS/Network/DTO/Trip/Response/CreateTravelResponseDTO.swift
+++ Going-iOS/Network/DTO/Trip/Response/CreateTravelResponseDTO.swift
@@ -8,18 +8,13 @@
 import Foundation
 
 struct CreateTravelResponseDTO : Response, DTO {
-    let tripId: Int64
+    let tripId: Int
     let title, startDate, endDate, code: String
     let day: Int
 }
 
 extension CreateTravelResponseDTO {
-    func toCreatingSuccessAppData() -> CreatingSuccessAppData {
-        return CreatingSuccessAppData(travelId: self.tripId,
-                                      travelTitle: self.title,
-                                      startDate: self.startDate,
-                                      endDate: self.endDate,
-                                      inviteCode: self.code,
-                                      dueDate: self.day)
+    func toAppData() -> CreateTravelResponseAppData {
+        return CreateTravelResponseAppData(tripId: self.tripId, title: self.title, startDate: self.startDate, endDate: self.endDate, code: self.code, day: self.day)
     }
 }
