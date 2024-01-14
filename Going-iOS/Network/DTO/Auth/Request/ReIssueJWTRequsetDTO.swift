8b47c45400ceb46f370cb4299d098412a4783a58
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-01-14 20:53:59 +0900

    [Chore] splashView 코드 수정(#101)

---
+++ Going-iOS/Network/DTO/Auth/Request/ReIssueJWTRequsetDTO.swift
@@ -0,0 +1,12 @@
+//
+//  ReIssueJWTRequsetDTO.swift
+//  Going-iOS
+//
+//  Created by 곽성준 on 1/14/24.
+//
+
+import Foundation
+
+//struct ReIssueJWTRequsetDTO: Request, DTO {
+//    let userId: ing
+//}
