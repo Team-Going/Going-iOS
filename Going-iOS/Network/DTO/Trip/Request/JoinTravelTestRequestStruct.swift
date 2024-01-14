0da2ff9d4a83c2765384957f29e0e43fe42d7317
0seoYun <yys020819@gmail.com>
2024-01-14 16:12:03 +0900

    [Feat] 여행참여테스트 Request / Response 구조체 생성 (#95)

---
+++ Going-iOS/Network/DTO/Trip/Request/JoinTravelTestRequestStruct.swift
@@ -0,0 +1,18 @@
+//
+//  JoinTravelTestRequestStruct.swift
+//  Going-iOS
+//
+//  Created by 윤영서 on 1/14/24.
+//
+
+import Foundation
+
+// MARK: - JoinTravelTest
+
+struct JoinTravelTestRequestStruct: Request {
+    let styleA: Int
+    let sytleB: Int
+    let styleC: Int
+    let styleD: Int
+    let sylteE: Int
+}
---
+++ Going-iOS/Network/DTO/Trip/Response/JoinTravelTestResponseStruct.swift
@@ -0,0 +1,14 @@
+//
+//  JoinTravelTestResponseStruct.swift
+//  Going-iOS
+//
+//  Created by 윤영서 on 1/14/24.
+//
+
+import Foundation
+
+// MARK: - JoinTravelTest
+
+struct JoinTravelTestResponseStruct: Response {
+    let tripId: Int
+}
