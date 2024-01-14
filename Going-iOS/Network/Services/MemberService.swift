28d5621ec9a7888f114929acb8292b777c7e68c9
0seoYun <yys020819@gmail.com>
2024-01-14 23:05:55 +0900

    [Feat] 여행친구 조회 Get 서비스 메서드 생성 (#102)

--- Going-iOS/Network/Services/MemberService.swift
+++ Going-iOS/Network/Services/MemberService.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class MemberService: Serviceable {
+    static let shared = MemberService()
+    
+    private init() {}
+    
+    /// 여행 친구 전체 조회 API
+    func getMemberInfo(tripId: Int) async throws -> MemberResponseStruct {
+        
+        let urlRequest = try NetworkRequest(path: "/api/trips/\(tripId)/participants", httpMethod: .get).makeURLRequest(networkType: .withJWT)
+        
+        let (data, _) = try await URLSession.shared.data(for: urlRequest)
+        
+        guard let model = try dataDecodeAndhandleErrorCode(data: data, decodeType: MemberResponseStruct.self) else { return MemberResponseStruct(participants: [], styles: []) }
+        
+        return model
+    }
+}
