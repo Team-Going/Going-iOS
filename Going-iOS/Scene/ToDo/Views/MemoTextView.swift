d967a21279e5ebdeb2361f391b136ca43b926937
seuriseuljjeok <heeseul09@naver.com>
2024-01-26 02:16:41 +0900

    [Add] 메모 뷰 추가 (#194)

--- Going-iOS/Scene/ToDo/Views/MemoTextView.swift
+++ Going-iOS/Scene/ToDo/Views/MemoTextView.swift
@@ -7,14 +7,172 @@
 
 import UIKit
 
+protocol MemoTextViewDelegate: AnyObject {
+    func checkMemoState()
+}
+
 class MemoTextView: UIView {
+    
+    private let memoLabel = DOOLabel(
+        font: .pretendard(.body2_bold),
+        color: .gray700,
+        text: StringLiterals.ToDo.memo
+    )
+    let memoTextView: UITextView = {
+        let tv = UITextView()
+        tv.backgroundColor = .white000
+        tv.textContainerInset = UIEdgeInsets(
+            top: ScreenUtils.getHeight(12),
+            left: ScreenUtils.getWidth(12),
+            bottom: ScreenUtils.getHeight(12),
+            right: ScreenUtils.getWidth(12)
+        )
+        tv.font = .pretendard(.body3_medi)
+        tv.textColor = .gray200
+        tv.layer.borderColor = UIColor.gray200.cgColor
+        tv.layer.cornerRadius = 6
+        tv.layer.borderWidth = 1
+        return tv
+    }()
+    private let countMemoCharacterLabel = DOOLabel(
+        font: .pretendard(.detail2_regular),
+        color: .gray200,
+        text: "0/1000"
+    )
+    private let memoWarningLabel: DOOLabel = {
+        let label = DOOLabel(font: .pretendard(.body3_medi), color: .red400)
+        label.isHidden = true
+        return label
+    }()
+    
+    
+    var memoTextViewCount: Int = 0
+    var memoTextviewPlaceholder: String = ""
+    weak var delegate: MemoTextViewDelegate?
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setHierarchy()
+        setLayout()
+        setDelegate()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setInquiryMemoStyle() {
+        guard let memotext = memoTextView.text?.count else {return}
+        memoTextView.layer.borderColor = memoTextView.text == "" ? UIColor.gray200.cgColor : UIColor.gray700.cgColor
+        memoTextView.textColor = memoTextView.text == "" ? UIColor.gray200 : UIColor.gray700
+        countMemoCharacterLabel.text = "\(memotext)/1000"
+        countMemoCharacterLabel.textColor = memoTextView.text == "" ? UIColor.gray200 : .gray700
+    }
 
+}
+
+private extension MemoTextView {
+    
+    func setHierarchy() {
+        self.addSubviews(memoLabel,
+                         memoTextView,
+                         memoWarningLabel,
+                         countMemoCharacterLabel
+        )
+    }
+    
+    func setLayout() {
+        memoLabel.snp.makeConstraints{
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(ScreenUtils.getHeight(24))
+        }
+        memoTextView.snp.makeConstraints{
+            $0.top.equalTo(memoLabel.snp.bottom).offset(ScreenUtils.getHeight(8))
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(ScreenUtils.getHeight(140))
+        }
+        countMemoCharacterLabel.snp.makeConstraints{
+            $0.top.equalTo(memoTextView.snp.bottom).offset(4)
+            $0.trailing.equalToSuperview().inset(ScreenUtils.getWidth(4))
+        }
+        memoWarningLabel.snp.makeConstraints {
+            $0.top.equalTo(memoTextView.snp.bottom).offset(4)
+            $0.leading.equalTo(memoTextView.snp.leading).offset(4)
+        }
+    }
+    
+    func setDelegate() {
+        memoTextView.delegate = self
+    }
+    
+    func memoTextViewBlankCheck() {
+        guard let textEmpty = memoTextView.text?.isEmpty else { return }
+        if textEmpty {
+            memoTextView.layer.borderColor = UIColor.gray200.cgColor
+            self.countMemoCharacterLabel.textColor = .gray200
+        } else {
+            memoTextView.layer.borderColor = UIColor.gray700.cgColor
+            self.countMemoCharacterLabel.textColor = .gray400
+        }
+    }
+    
+    func textViewCountCheck() {
+        
+        guard let text = memoTextView.text else { return }
+        
+        if text.count > 1000 {
+            memoTextView.layer.borderColor = UIColor.red500.cgColor
+            countMemoCharacterLabel.textColor = .red500
+            memoWarningLabel.text = "메모는 1000자를 초과할 수 없습니다."
+            memoWarningLabel.isHidden = false
+        } else {
+            memoWarningLabel.isHidden = true
+            
+        }
+    }
+}
+
+extension MemoTextView: UITextViewDelegate {
+    
+    func textViewDidBeginEditing (_ textView: UITextView) {
+        textView.becomeFirstResponder()
+        
+        if textView.text == memoTextviewPlaceholder {
+            textView.text = ""
+            textView.textColor = .gray700
+        }
+        textViewCountCheck()
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if let char = text.cString(using: String.Encoding.utf8) {
+            let isBackSpace = strcmp(char, "\\b")
+            if isBackSpace == -92 {
+                return true
+            }
+        }
+        
+        return true
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            textView.text = memoTextviewPlaceholder
+            textView.textColor = .gray200
+            textViewCountCheck()
+        }
+        
+        
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        if textView == memoTextView {
+            let memoTextViewCount = textView.text.count
+            countMemoCharacterLabel.text = "\(memoTextViewCount)/1000"
+            memoTextViewBlankCheck()
+            textViewCountCheck()
+            self.delegate?.checkMemoState()
+        }
+    }
 }
