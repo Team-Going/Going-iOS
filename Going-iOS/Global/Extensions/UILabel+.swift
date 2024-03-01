b235a97c2a842e2895ac3d4d78bc7d718ac9b51b
0seoYun <yys020819@gmail.com>
2024-03-02 03:00:03 +0900

    [Feat] UILabel 행간 설정 메서드 extension 추가 (#201)

--- Going-iOS/Global/Extensions/UILabel+.swift
+++ Going-iOS/Global/Extensions/UILabel+.swift
@@ -7,11 +7,23 @@
 import UIKit
 
 extension UILabel {
-        func labelWithImg(composition: NSAttributedString...) {
-            let attributedString = NSMutableAttributedString()
-            for i in composition {
+    func labelWithImg(composition: NSAttributedString...) {
+        let attributedString = NSMutableAttributedString()
+        for i in composition {
             attributedString.append(i)
-            }
+        }
         self.attributedText = attributedString
+    }
+    
+    func setLineSpacing(spacing: CGFloat) {
+        guard let text = text else { return }
+        
+        let attributeString = NSMutableAttributedString(string: text)
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = spacing
+        attributeString.addAttribute(.paragraphStyle,
+                                     value: style,
+                                     range: NSRange(location: 0, length: attributeString.length))
+        attributedText = attributeString
     }
 }
