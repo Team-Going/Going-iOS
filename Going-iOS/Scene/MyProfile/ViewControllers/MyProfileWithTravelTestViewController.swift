405aa6566bdf658d9578acd04d82569bcdfc7c40
0seoYun <yys020819@gmail.com>
2024-03-05 02:04:16 +0900

    [Feat] 여행 내 내프로필 조회뷰 구현 (#209)

--- Going-iOS/Scene/MyProfile/ViewControllers/MyProfileWithTravelTestViewController.swift
+++ Going-iOS/Scene/MyProfile/ViewControllers/MyProfileWithTravelTestViewController.swift
@@ -1,7 +1,279 @@
+//
+//  MyProfileWithTravelTestViewController.swift
+//  Going-iOS
+//
+//  Created by 윤영서 on 3/3/24.
+//
+
+
+import UIKit
+
+import Photos
+import SnapKit
+
+final class MyProfileWithTravelTestViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    var segmentIndex: Int = 0
+    
+    private var testResultData: UserTypeTestResultAppData? {
+        didSet {
+            guard let data = testResultData else { return }
+            self.myProfileTopView.profileImageView.image = data.profileImage
+            self.userTestResultScrollView.resultImageView.image = data.typeImage
+            self.userTestResultScrollView.myResultView.resultViewData = data
+        }
+    }
+    
+    private var testResultIndex: Int?
+    
+    // MARK: - UI Properties
+    
+    private lazy var navigationBar = DOONavigationBar(self, type: .backButtonWithTitle("내 여행 프로필"))
+    
+    private lazy var saveButton: UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(resource: .btnSave), for: .normal)
+        btn.addTarget(self, action: #selector(saveImageButtonTapped), for: .touchUpInside)
+        return btn
+    }()
+    
+    private let naviUnderLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(resource: .gray100)
+        return view
+    }()
+    
+    private let myProfileTopView = MyProfileTopView()
+    
+    private let profileWithTravelTestHeaderView = ProfileWithTravelTestHeaderView()
+    
+    private let emptyUserTestView = EmptyUserTestView()
+    
+    private let userTestResultScrollView = UserTestResultScrollView()
+
+    // MARK: - Life Cycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setStyle()
+        setHierarchy()
+        setLayout()
+        setDelegate()
+        setSegmentDidChange()
+        setSegment()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getUserProfile()
+    }
+}
+
+// MARK: - Private Methods
+
+private extension MyProfileWithTravelTestViewController {
+    func setStyle() {
+        view.backgroundColor = UIColor(resource: .white000)
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    func setHierarchy() {
+        view.addSubviews(navigationBar,
+                         naviUnderLineView,
+                         myProfileTopView,
+                         profileWithTravelTestHeaderView,
+                         emptyUserTestView,
+                         userTestResultScrollView)
+        
+        navigationBar.addSubview(saveButton)
+    }
+    
+    func setLayout() {
+        navigationBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(ScreenUtils.getHeight(50))
+        }
+        
+        saveButton.snp.makeConstraints {
+            $0.centerY.equalTo(navigationBar)
+            $0.trailing.equalToSuperview().inset(10)
+        }
+        
+        naviUnderLineView.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(1)
+        }
+        
+        myProfileTopView.snp.makeConstraints {
+            $0.top.equalTo(naviUnderLineView.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(ScreenUtils.getHeight(109))
+        }
+        
+        profileWithTravelTestHeaderView.snp.makeConstraints {
+            $0.top.equalTo(myProfileTopView.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(ScreenUtils.getHeight(50))
+        }
+        
+        emptyUserTestView.snp.makeConstraints {
+            $0.top.equalTo(profileWithTravelTestHeaderView.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-6)
+        }
+        
+        userTestResultScrollView.snp.makeConstraints {
+            $0.top.equalTo(profileWithTravelTestHeaderView.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }    
+    
+    func setDelegate() {
+        userTestResultScrollView.myResultView.delegate = self
+    }
+    
+    func setSegmentDidChange() {
+        self.didChangeValue(sender: self.profileWithTravelTestHeaderView.segmentedControl)
+    }
+    
+    func setSegment() {
+        profileWithTravelTestHeaderView.segmentedControl.addTarget(self,
+                                                       action: #selector(didChangeValue(sender:)),
+                                                       for: .valueChanged)
+    }
+    
+    
+    func saveImage() {
+        guard let saveImage = testResultData?.phoneSaveImage else {
+            DOOToast.show(message: "이미지 저장 오류", insetFromBottom: 80)
+            return
+        }
+        
+        UIImageWriteToSavedPhotosAlbum(saveImage, self, nil, nil)
+        DOOToast.show(message: "이미지로 저장되었어요\n친구에게 내 캐릭터를 공유해보세요", insetFromBottom: 114)
+    }
+    
+    func showPermissionAlert() {
+        // PHPhotoLibrary.requestAuthorization() 결과 콜백이 main thread로부터 호출되지 않기 때문에
+        // UI처리를 위해 main thread내에서 팝업을 띄우도록 함.
+        DispatchQueue.main.async {
+            
+            let alert = UIAlertController(title: nil, message: "설정으로 이동하여 권한을 허용해 주세요. 내 여행 프로필에서 다시 저장할 수 있어요.", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "설정으로 이동", style: .default, handler: { _ in
+                UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+            }))
+            alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+            
+            self.present(alert, animated: true)
+        }
+    }
+    
+    // MARK: - @objc methods
+    
+    @objc func saveImageButtonTapped() {
+        checkAccess()
+    }
+    
+    @objc
+    func didChangeValue(sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            self.segmentIndex = 0
+            userTestResultScrollView.isHidden = false
+        } else {
+            self.segmentIndex = 1
+            userTestResultScrollView.isHidden = true
+        }
+    }
+}
+
+extension MyProfileWithTravelTestViewController: CheckPhotoAccessProtocol {
+    func checkAccess() {
+        switch PHPhotoLibrary.authorizationStatus(for: .addOnly) {
+            
+        case .notDetermined, .denied:
+            UserDefaults.standard.set(false, forKey: "photoPermissionKey")
+            PHPhotoLibrary.requestAuthorization(for: .addOnly) { [weak self] status in
+                switch status {
+                case .authorized, .limited:
+                    UserDefaults.standard.set(true, forKey: "photoPermissionKey")
+                case .denied:
+                    DispatchQueue.main.async {
+                        self?.showPermissionAlert()
+                    }
+                default:
+                    print("그 밖의 권한이 부여 되었습니다.")
+                }
+            }
+            
+        case .restricted, .limited, .authorized:
+            saveImage()
+            UserDefaults.standard.set(true, forKey: "photoPermissionKey")
+        @unknown default:
+            print("unKnown")
+        }
+    }
+}
+
 extension MyProfileWithTravelTestViewController: TestResultViewDelegate {
     func backToTestButton() {
         let vc = UserTestSplashViewController()
         self.navigationController?.pushViewController(vc, animated: false)
         vc.beforeVC = "myProfile"
+    }
+}
+
+// MARK: - Network
+
+extension MyProfileWithTravelTestViewController {
+    func getUserProfile() {
+        Task {
+            do {
+                let profileData = try await TravelService.shared.getProfileInfo()
+                self.testResultIndex = profileData.result
+                self.myProfileTopView.userDescriptionLabel.text = profileData.intro
+                self.myProfileTopView.userNameLabel.text = profileData.name
+                
+                guard let index = testResultIndex else { return }
+                self.testResultData = UserTypeTestResultAppData.dummy()[index]
+            }
+            catch {
+                guard let error = error as? NetworkError else { return }
+                handleError(error)
+            }
+        }
+    }
+}
+
+extension MyProfileWithTravelTestViewController: ViewControllerServiceable {
+    func handleError(_ error: NetworkError) {
+        switch error {
+        case .unAuthorizedError, .reIssueJWT:
+            DOOToast.show(message: "토큰만료, 재로그인필요", insetFromBottom: 80)
+            let nextVC = LoginViewController()
+            self.navigationController?.pushViewController(nextVC, animated: true)
+        case .userState(_, let message):
+            DOOToast.show(message: message, insetFromBottom: 80)
+        default:
+            DOOToast.show(message: error.description, insetFromBottom: 80)
+        }
+    }
+}
+
+extension MyProfileWithTravelTestViewController {
+    func reIssueJWTToken() {
+        Task {
+            do {
+                try await AuthService.shared.postReIssueJWTToken()
+            }
+            catch {
+                guard let error = error as? NetworkError else { return }
+                handleError(error)
+            }
+        }
     }
 }
